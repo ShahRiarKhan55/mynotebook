9da2cd41eae6b3ad669471dacf993c24522114cc
ShahRiarKhan55 <siaabid1234@gmail.com>
2023-07-14 03:35:07 +0600

    4th commit

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,9 +3,10 @@ const User = require("../models/user");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
-const JWT_SECRET = "adibisgood";
-const jwt = require("jsonwebtoken");
 
+const jwt = require("jsonwebtoken");
+const fetchuser = require("../middleware/fetchuser");
+const JWT_SECRET = "adibisgood";
 // Create a new user using: POST "/api/auth"
 router.post(
   "/createuser",
@@ -24,9 +25,9 @@ router.post(
       return res.status(400).json({ error: "Email exist" });
     }
     try {
-      var salt = await bcrypt.genSalt(10);
+      const salt = await bcrypt.genSalt(10);
 
-      var secPass = await bcrypt.hash(req.body.password, salt);
+      const secPass = await bcrypt.hash(req.body.password, salt);
 
       user = await User.create({
         name: req.body.name,
@@ -49,5 +50,46 @@ router.post(
     }
   }
 );
-
+router.post(
+  "/login",
+  [
+    body("email", "Enter valid email").isEmail(),
+    body("password", "no blank password").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ error: "Please enter correct password" });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res.status(400).json({ error: "Please enter correct password" });
+      }
+      const data = {
+        user: { id: user.id },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json({ authtoken });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send(" internal server error occurred");
+    }
+  }
+);
+router.post("/getuser", fetchuser, async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send(" internal server error occurred");
+  }
+});
 module.exports = router;
